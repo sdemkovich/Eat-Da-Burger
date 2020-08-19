39235ec1d2e29f1ecbf529182b10992919d22cd3
Sveta Demkovich <svetlana.demkovich@gmail.com>
2020-08-18 22:07:15 -0700

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,8 @@
-INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("Katsu Burger");
-INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bleu Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("SoCal Veggie Burger");
 INSERT INTO burgers (burger_name) VALUES ("Hamburger");
-INSERT INTO burgers (burger_name) VALUES ("BBQ Burger");
-INSERT INTO burgers (burger_name) VALUES ("Big Mac");
+INSERT INTO burgers (burger_name) VALUES ("One-Eye Burger");
 INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
 
-UPDATE burgers SET devoured = true WHERE burger_name = "Bacon Cheeseburger";
+UPDATE burgers SET devoured = true WHERE burger_name = "Cheeseburger";
