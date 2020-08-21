e1ab09f33f7e72d8007f4335c21035ff2a36ac0d
Sveta Demkovich <svetlana.demkovich@gmail.com>
2020-08-20 20:17:42 -0700

    update connections

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     var connection = mysql.createConnection({
-        host: "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "	dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
-        user: "ebe01ysajq8b064x",
-        password: process.env.MYSQL_PASSWORD,
-        database: "gt48vxrnbgn9k91b",
+        user: "loqzeqetcmsgt08w",
+        password: "iwbhn2lo23bvzdwk",
+        database: "n5u85sdcfz3izlcy",
     });
 }
 
